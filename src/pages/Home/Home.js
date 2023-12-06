14a3197dfa95f3082f081b2dec106bb21abb9c67
raspiotr <raspiotr@gmail.com>
2023-12-06 22:51:38 +0100

    text decoration

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -9,6 +9,7 @@ export default function Home() {
       </Helmet>
       <h1 className={css.Title}>Contacts List App - Welcome!</h1>
       <img
+        className={css.Image}
         src="https://cdn3d.iconscout.com/3d/premium/thumb/phone-book-6772623-5573716.png"
         alt="Pbonebook"
       />
